e280c6b4c4f282b2275e69cb1b7aa65ca1517604
Kelvin Appiah <kelpyshades060904@gmail.com>
2023-09-25 01:58:53 +0000

    Initial commit

---
+++ Connect.js
@@ -0,0 +1,11 @@
+
+function myFunction1() {
+    document.getElementById("container").style.display = "none";
+    document.getElementById("textbox").style.display = "none";
+    document.getElementById("textandvid").style.display = "block";
+}
+function myFunction2() {
+    document.getElementById("container").style.display = "block";
+    document.getElementById("textbox").style.display = "none";
+    document.getElementById("textandvid").style.display = "none";
+}
---
+++ README.txt
@@ -0,0 +1,2 @@
+link to site
+https://unect-10617.web.app
